3bc4ee69d0a224ff14f11b3dc92f9a9ea048d148
jesselangdon <jesselangdon@gmail.com>
2017-04-27 12:23:30 -0700

    Added network.py tests for braids, headwaters, and merge_subgraphs.

--- lib/network.py
+++ lib/network.py
@@ -70,7 +70,6 @@ def update_attribute(G, attrb_name, attrb_value):
             print "ERROR: Attribute type does not exist in the network"
     except:
         print "ERROR: Missing an input parameter"
-        raise Exception
     return
 
 
@@ -107,15 +106,20 @@ def get_outflow_edges(G, attrb_field):
     :param attrb_type: name of the attribute field
     :return outflow_G: graph with new headwater attribute
     """
-    out_dict = G.out_degree()
-    # find the outflow node (should have zero outgoing edges, right?)
-    outflow_node = list(dict((k, v) for k, v in out_dict.iteritems() if v == 0))
-    # get the edge that is connected to outflow node
-    outflow_edge = G.in_edges(outflow_node, data=True)
-    outflow_G = nx.DiGraph(outflow_edge)
-    # set reach_type attribute for outflow and headwater edges
-    update_attribute(attrb_field, "outflow")
-    return outflow_G
+    if nx.is_directed(G):
+        out_dict = G.out_degree()
+        # find the outflow node (should have zero outgoing edges, right?)
+        outflow_node = list(dict((k, v) for k, v in out_dict.iteritems() if v == 0))
+        # get the edge that is connected to outflow node
+        outflow_edge = G.in_edges(outflow_node, data=True)
+        outflow_G = nx.DiGraph(outflow_edge)
+        # set reach_type attribute for outflow and headwater edges
+        update_attribute(outflow_G, attrb_field, "outflow")
+        return outflow_G
+    else:
+        print "ERROR: Graph is not directed."
+        outflow_G = nx.null_graph()
+        return outflow_G
 
 
 def get_headwater_edges(G, attrb_field):
@@ -124,12 +128,17 @@ def get_headwater_edges(G, attrb_field):
     :param attrb_field: name of the attribute field
     :return headwater_G: graph with new attribute
     """
-    in_dict = G.in_degree()
-    headwater_nodes = list(dict((k, v) for k, v in in_dict.iteritems() if v == 0))
-    headwater_edges = G.out_edges(headwater_nodes, data=True)
-    headwater_G = nx.DiGraph(headwater_edges)
-    update_attribute(attrb_field, "headwater")
-    return headwater_G
+    if nx.is_directed(G):
+        in_dict = G.in_degree()
+        headwater_nodes = list(dict((k, v) for k, v in in_dict.iteritems() if v == 0))
+        headwater_edges = G.out_edges(headwater_nodes, data=True)
+        headwater_G = nx.DiGraph(headwater_edges)
+        update_attribute(G, attrb_field, "headwater")
+        return headwater_G
+    else:
+        print "ERROR: Graph is not directed."
+        headwater_G = nx.null_graph()
+        return headwater_G
 
 
 def get_braid_edges(G, attrb_field):
@@ -138,31 +147,45 @@ def get_braid_edges(G, attrb_field):
     :param attrb_field: name of the attribute field
     :return braid_G: graph with new attribute
     """
-    UG = G.to_undirected()
-    print "Number of edges in undirected graph: " + str(UG.number_of_edges())
-    braid_G = nx.DiGraph()
-    try:
+    if nx.is_directed(G):
+        UG = G.to_undirected()
+        print "Number of edges in undirected graph: " + str(UG.number_of_edges())
+        braid_G = nx.DiGraph()
         print "Finding cycles in undirected graph..."
         list_cycles = nx.cycle_basis(UG)
-        print "Total number of potential braids: " + len(list_cycles)
-    except:
-        print "ERROR: no biconnected segments found"
-        exit(0)
-    for cycles in list_cycles:
-        cycle_edges = zip(cycles, cycles[1:] + cycles[:1])
-        print len(cycle_edges)
-        for u, v, d in G.edges(nbunch=cycles, data=True):
-            braid_G.add_edge(u, v, d)
-    update_attribute(braid_G, attrb_field, "braid")
-    return braid_G
+        if len(list_cycles) == 0:
+            print "ERROR: no braids found!"
+            braid_G = nx.null_graph()
+            return braid_G
+        else:
+            print "Total number of potential braids: " + str(len(list_cycles))
+            for cycles in list_cycles:
+                #cycle_edges = zip(cycles, cycles[1:] + cycles[:1])
+                for u, v, d in G.edges(nbunch=cycles, data=True):
+                    braid_G.add_edge(u, v, d)
+            update_attribute(braid_G, attrb_field, "braid")
+            return braid_G
+    else:
+        print "ERROR: Graph is not directed."
+        braid_G = nx.null_graph()
+        return braid_G
 
 
-def merge_subgraphs(G, list_G):  # this needs to be refactored to handle a list of graphs
+def merge_subgraphs(G, outflow_G, headwater_G, braid_G):
     """
     Join all subgraphs with the main graph
     :param list_G: list of subgraphs with reach type attribute added
     :return G_compose: final graph output with all reach type attributes included
     """
-    list_G.insert(0, G)
-    compose_G = nx.compose_all(list_G)
+
+    # SG = list_G.pop(0)
+    # compose_G = nx.compose(G, SG)
+    # if len(list_G) > 0:
+    #     merge_subgraphs(compose_G, list_G)
+
+    # this is not ideal, but fancy recursion didn't work
+    G1 = nx.compose(G, outflow_G)
+    G2 = nx.compose(G1, headwater_G)
+    compose_G = nx.compose(G2, braid_G)
+
     return compose_G
