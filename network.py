2cad11e88df17cdb7870c53e4c5dcfc0ec9987d0
jesselangdon <jesselangdon@gmail.com>
2017-06-23 14:17:24 -0700

    Added function for finding flow errors. Only paritally works.

--- network.py
+++ network.py
@@ -8,7 +8,6 @@
 import os.path
 import networkx as nx
 
-
 def import_shp(file_path):
     if os.path.isfile(file_path) and file_path.endswith(".shp"):
         DG = nx.read_shp(file_path, simplify=True)
@@ -332,3 +331,68 @@ def merge_subgraphs(G, outflow_G, headwater_G, braid_G):
     compose_G = nx.compose(G2, braid_G)
 
     return compose_G
+
+
+# def check_flow_dir(G, start_node):
+#
+#     RG = nx.DiGraph.reverse(G, copy=True)
+#     add_attribute(RG, "error_flow", 0) # add 'default' reach type
+#
+#     # Get first downstream node
+#     successor_dict = nx.dfs_successors(RG, start_node)    predecessor_dict = nx.dfs_predecessors(G, start_node)
+#
+#     RG_edges = RG.edges(data=True)
+#     upstream_edges = []
+#     for e in RG_edges:
+#         if e[0] not in successor_dict.keys():
+#
+#             upstream_edges.append(e)
+#
+#     # add new "FlowDir" attribute, based on bad_edges list
+#     upstream_graph = nx.DiGraph(upstream_edges)
+#     # set reach_type attribute for outflow and headwater edges
+#     update_attribute(upstream_graph, "error_flow", 1)
+#     compose_G = nx.compose(RG, upstream_graph)
+#     return compose_G
+
+
+def flow_errors(G, src, ud=None):
+    """Returns the first edges that do not conform to the flow direction
+    implicit in defined source node.
+    G: target digraph
+    src: source nodes
+    ud: undirected graph (faster iteration with setdirection)
+    stopnodes: break points in the network
+    """
+    RG = nx.reverse(G, copy=True)
+    badedges = []
+    gnodes = list(nx.dfs_preorder_nodes(RG, src))
+    if not ud:
+        ud = RG.to_undirected()
+    connected = RG.edges(nx.dfs_tree(ud, src).nodes())
+    for edge in connected:
+        start = edge[0]
+        end = edge[1]
+        if end in gnodes and start not in gnodes:
+                badedges.append(RG.edges(start)[0])
+    return badedges
+
+
+def findnodewithID(id):
+    """
+    One line helper function to find a node with a given ID
+    :param id:
+    :return:
+    """
+    return next(iter([e for e in G.edges_iter() if G.get_edge_data(*e)['OBJECTID'] == id]), None)
+
+
+# TEST
+inshp = r"C:\JL\Testing\pyGNAT\NetworkFeatures\In\NHD_Flow_Direction.shp"
+outdir = r"C:\JL\Testing\pyGNAT\NetworkFeatures\Out"
+G = nx.read_shp(inshp)
+start_node = findnodewithID(2695.0)
+error_edges = flow_errors(G, start_node[1])
+error_G = nx.DiGraph(error_edges)
+export_shp(error_G, inshp, outdir)
+#  TEST
